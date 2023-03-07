5ce46ca906207987e1d04d1ef00d858f2610944a
BogettiGian <gianbogetti7@hotmail.com>
2023-03-07 09:14:11 -0300

    Improve CSV generator

--- src/utils.py
+++ src/utils.py
@@ -49,65 +49,33 @@ def dict_to_game(game_dict: dict):
 
 
 def get_pieces_array(piece_map: dict):
-    w_rs = [-1] * 4
-    w_rs_i = 0
-    w_ns = [-1] * 4
-    w_ns_i = 0
-    w_bs = [-1] * 4
-    w_bs_i = 0
-    w_q = [-1] * 5
-    w_q_i = 0
-    w_k = [-1]
-    w_ps = [-1] * 8
-    w_ps_i = 0
-
-    b_rs = [-1] * 4
-    b_rs_i = 0
-    b_ns = [-1] * 4
-    b_ns_i = 0
-    b_bs = [-1] * 4
-    b_bs_i = 0
-    b_q = [-1] * 5
-    b_q_i = 0
-    b_k = [-1]
-    b_ps = [-1] * 8
-    b_ps_i = 0
+    board = [0] * 64
 
     for key in piece_map.keys():
         match str(piece_map[key]):
             case 'r':
-                b_rs[b_rs_i] = key
-                b_rs_i += 1
+                board[key] = - chess.ROOK
             case 'n':
-                b_ns[b_ns_i] = key
-                b_ns_i += 1
+                board[key] = - chess.KNIGHT
             case 'b':
-                b_bs[b_bs_i] = key
-                b_bs_i += 1
+                board[key] = - chess.BISHOP
             case 'q':
-                b_q[b_q_i] = key
-                b_q_i += 1
+                board[key] = - chess.QUEEN
             case 'k':
-                b_k[0] = key
+                board[key] = - chess.KING
             case 'p':
-                b_ps[b_ps_i] = key
-                b_ps_i += 1
+                board[key] = - chess.PAWN
             case 'R':
-                w_rs[w_rs_i] = key
-                w_rs_i += 1
+                board[key] = chess.ROOK
             case 'N':
-                w_ns[w_ns_i] = key
-                w_ns_i += 1
+                board[key] = chess.KNIGHT
             case 'B':
-                w_bs[w_bs_i] = key
-                w_bs_i += 1
+                board[key] = chess.BISHOP
             case 'Q':
-                w_q[w_q_i] = key
-                w_q_i += 1
+                board[key] = chess.QUEEN
             case 'K':
-                w_k[0] = key
+                board[key] = chess.KING
             case 'P':
-                w_ps[w_ps_i] = key
-                w_ps_i += 1
+                board[key] = chess.PAWN
 
-    return w_rs + w_ns + w_bs + w_q + w_k + w_ps + b_rs + b_ns + b_bs + b_q + b_k + b_ps
+    return board
