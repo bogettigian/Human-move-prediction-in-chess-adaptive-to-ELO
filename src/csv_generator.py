77a0e290e01a33b8e132bbd2a29b03f19dc9c635
BogettiGian <gianbogetti7@hotmail.com>
2023-03-12 23:46:34 -0300

    Improve csv generator

--- src/csv_generator.py
+++ src/csv_generator.py
@@ -1,8 +1,7 @@
 import argparse
 import os
 import time
-import chess
-import pandas as pd
+import numpy as np
 
 import database
 import utils
@@ -26,104 +25,22 @@ if __name__ == "__main__":
         os.makedirs(os.path.dirname(path))
 
     collection = database.get_database(connection_string, database_name, collection_name)
+    filter = {}
 
     start_time = time.time()
     total_records = 0
     saved_records = 0
 
-    columns = list(range(1, 65)) + utils.generate_columns('_1b') + utils.generate_columns(
-        '_2b') + utils.generate_columns('_3b') + [
-                  'active_color',
-                  'castling_K',
-                  'castling_Q',
-                  'castling_k',
-                  'castling_q',
-                  'en_passant',
-                  'half-move_clock',
-                  'threefold',
-                  'move_number',
-                  'elo',
-                  'game_time',
-                  'increase',
-                  'time_left',
-                  'from_square',
-                  'to_square',
-                  'checkmate_count',
-                  'valuation',
-                  'delta_valuation']
+    with open(path, 'w') as f:
+        np.savetxt(f, utils.columns(), delimiter=',', fmt='%s')
 
-    filter = {}
-    result = collection.find(filter)
-
-    data = []
-    for game_dict in result:
-        game = utils.dict_to_game(game_dict)
-        board_game = game.board()
+    for game_dict in collection.find(filter):
+        data, records = utils.game_dict_to_array(game_dict)
         total_records += 1
-        move_3b = [0] * 69
-        move_2b = [0] * 69
-        move_1b = [0] * 69
-        prev_val = 0
+        saved_records += records
 
-        for node in game.mainline():
-            row = utils.get_pieces_array(board_game.piece_map())
-            row = row + move_1b + move_2b + move_3b
-
-            row.append(int(board_game.turn))
-            row.append(int(board_game.has_kingside_castling_rights(chess.WHITE)))
-            row.append(int(board_game.has_queenside_castling_rights(chess.WHITE)))
-            row.append(int(board_game.has_kingside_castling_rights(chess.BLACK)))
-            row.append(int(board_game.has_kingside_castling_rights(chess.BLACK)))
-            row.append(0 if not board_game.has_legal_en_passant() else
-                       chess.parse_square((board_game.fen().split()[3])))
-            row.append(board_game.halfmove_clock)
-            row.append(int(board_game.can_claim_threefold_repetition()))
-            row.append(board_game.fullmove_number)
-
-            row.append(game.headers['WhiteElo'] if board_game.turn else game.headers['BlackElo'])
-            game_time, increase = game.headers['TimeControl'].split('+')
-            row.append(game_time)
-            row.append(increase)
-            row.append(node.clock() if node.clock() is not None else node.emt())
-            row.append(node.move.from_square)
-            row.append(node.move.to_square)
-
-            board_game.push(node.move)
-            if board_game.is_checkmate() and not board_game.turn:
-                row.append(0)
-                if not board_game.turn:
-                    val = utils.CHECKMATE_VAL
-                else:
-                    val = - utils.CHECKMATE_VAL
-            elif node.eval() is not None:
-                if node.eval().pov(not board_game.turn).is_mate():
-                    row.append(abs(node.eval().pov(not board_game.turn).mate()))
-                    if not board_game.turn:
-                        val = utils.CHECKMATE_VAL
-                    else:
-                        val = - utils.CHECKMATE_VAL
-                else:
-                    row.append(-1)
-                    val = node.eval().pov(not board_game.turn).score()
-            else:
-                # The following movements are not analyzed.
-                break
-
-            row.append(val)
-            if not board_game.turn:
-                row.append(val - prev_val)
-            else:
-                row.append((val - prev_val) * -1)
-
-            move_1b = row[:64] + row[-5:]
-            move_2b = move_1b
-            move_3b = move_2b
-            prev_val = val
-            saved_records += 1
-            data.append(row)
-
-    df = pd.DataFrame(data=data, columns=columns)
-    df.to_csv(path)
+        with open(path, 'a') as f:
+            np.savetxt(f, data, delimiter=',', fmt='%s')
 
     print(f'Total games read: {total_records}')
     print(f'Total games saved: {saved_records}')
--- src/utils.py
+++ src/utils.py
@@ -97,3 +97,97 @@ def generate_columns(prefix: str):
     col_names.append('delta_valuation' + prefix)
 
     return col_names
+
+
+def columns():
+    return [list(range(1, 65)) + generate_columns('_1b') + generate_columns('_2b') + generate_columns('_3b') + [
+        'active_color',
+        'castling_K',
+        'castling_Q',
+        'castling_k',
+        'castling_q',
+        'en_passant',
+        'half-move_clock',
+        'threefold',
+        'move_number',
+        'elo',
+        'game_time',
+        'increase',
+        'time_left',
+        'from_square',
+        'to_square',
+        'checkmate_count',
+        'valuation',
+        'delta_valuation']]
+
+
+def game_dict_to_array(game_dict: dict):
+    data = []
+    saved_records = 0
+
+    game = dict_to_game(game_dict)
+    board_game = game.board()
+
+    move_3b = [0] * 69
+    move_2b = [0] * 69
+    move_1b = [0] * 69
+    prev_val = 0
+
+    for node in game.mainline():
+        row = get_pieces_array(board_game.piece_map())
+        row = row + move_1b + move_2b + move_3b
+
+        row.append(int(board_game.turn))
+        row.append(int(board_game.has_kingside_castling_rights(chess.WHITE)))
+        row.append(int(board_game.has_queenside_castling_rights(chess.WHITE)))
+        row.append(int(board_game.has_kingside_castling_rights(chess.BLACK)))
+        row.append(int(board_game.has_kingside_castling_rights(chess.BLACK)))
+        row.append(0 if not board_game.has_legal_en_passant() else
+                   chess.parse_square((board_game.fen().split()[3])))
+        row.append(board_game.halfmove_clock)
+        row.append(int(board_game.can_claim_threefold_repetition()))
+        row.append(board_game.fullmove_number)
+
+        row.append(int(game.headers['WhiteElo']) if board_game.turn else int(game.headers['BlackElo']))
+        game_time, increase = game.headers['TimeControl'].split('+')
+        row.append(game_time)
+        row.append(increase)
+        row.append(int(node.clock()) if node.clock() is not None else int(node.emt()))
+        row.append(node.move.from_square)
+        row.append(node.move.to_square)
+
+        board_game.push(node.move)
+        if board_game.is_checkmate() and not board_game.turn:
+            row.append(0)
+            if not board_game.turn:
+                val = CHECKMATE_VAL
+            else:
+                val = - CHECKMATE_VAL
+        elif node.eval() is not None:
+            if node.eval().pov(not board_game.turn).is_mate():
+                row.append(abs(node.eval().pov(not board_game.turn).mate()))
+                if not board_game.turn:
+                    val = CHECKMATE_VAL
+                else:
+                    val = - CHECKMATE_VAL
+            else:
+                row.append(-1)
+                val = node.eval().pov(not board_game.turn).score()
+        else:
+            # The following movements are not analyzed.
+            break
+
+        row.append(val)
+        if not board_game.turn:
+            row.append(val - prev_val)
+        else:
+            row.append((val - prev_val) * -1)
+
+        move_1b = row[:64] + row[-5:]
+        move_2b = move_1b
+        move_3b = move_2b
+        prev_val = val
+        saved_records += 1
+        data.append(row)
+
+    return data, saved_records
