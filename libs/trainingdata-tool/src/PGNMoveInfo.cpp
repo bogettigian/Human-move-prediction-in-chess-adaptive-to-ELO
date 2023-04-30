e566c60077b7a74348ab8b7e5b5045cee5751707
BogettiGian <gianbogetti7@hotmail.com>
2023-04-30 19:05:24 -0300

    Update submodules dir

--- libs/trainingdata-tool/src/PGNMoveInfo.cpp
+++
@@ -1,9 +0,0 @@
-#include "PGNMoveInfo.h"
-
-#include <cstring>
-
-PGNMoveInfo::PGNMoveInfo(char* move, char* comment, char* nag) {
-  strncpy(this->move, move, PGN_STRING_SIZE);
-  strncpy(this->comment, comment, PGN_STRING_SIZE);
-  strncpy(this->nag, nag, PGN_STRING_SIZE);
-}
