6942e6cad61873c859fa72efef4df674fb27b12f
BogettiGian <gianbogetti7@hotmail.com>
2023-07-31 17:28:11 -0300

    Update pgn generator

--- src/pgn_generator.py
+++ src/pgn_generator.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import os
 import time
 
@@ -12,6 +13,10 @@ if __name__ == "__main__":
     parser.add_argument('-d', '--database', default='chess-mongo', help="Database name")
     parser.add_argument('-t', '--collection', default='moves', help="Collection name")
     parser.add_argument('-p', '--path', default='./../data/pgn/data.pgn', help="Data output")
+
+    parser.add_argument('-f', '--filter', default={}, type=json.loads, help="Database query filter")
+    parser.add_argument('-s', '--skip', default=0, type=int, help="Database query skip")
+    parser.add_argument('-l', '--limit', default=0, type=int, help="Database query limit")
     args = vars(parser.parse_args())
 
     connection_string = args['connection_string']
@@ -19,15 +24,18 @@ if __name__ == "__main__":
     collection_name = args['collection']
     path = args['path']
 
+    db_filter = args['filter']
+    db_skip = args['skip']
+    db_limit = args['limit']
+
     if not os.path.exists(os.path.dirname(path)):
         os.makedirs(os.path.dirname(path))
 
     start_time = time.time()
     saved_records = 0
 
-    filter = {}
     collection = database.get_database(connection_string, database_name, collection_name)
-    for game_dict in collection.find(filter):
+    for game_dict in collection.find(db_filter).sort({'$natural': 1}).skip(db_skip).limit(db_limit):
         game = utils.dict_to_game(game_dict)
         saved_records += 1
 
