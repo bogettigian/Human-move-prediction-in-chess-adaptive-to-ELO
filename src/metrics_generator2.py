3e9ad34df87878c8068c9278ed624c1406a1f00e
Gianfranco Bogetti <gianbogetti7@hotmail.com>
2025-06-26 08:42:05 -0300

    new metric calculator

---
+++ src/metrics_generator2.py
@@ -0,0 +1,71 @@
+import argparse
+import pandas as pd
+from sklearn import metrics
+
+
+def split_in_matches(data):
+    data.reset_index(drop=True)
+    df_result = []
+    init = 0
+    for index in data.index:
+        if data.iloc[index]['is_end']:
+            if index > init + 10:
+                df_result.append(data.iloc[init:index + 1])
+            init = index + 1
+    return df_result
+
+
+def move_play_time(data, time):
+    for index, row in data.iterrows():
+        if row['clock'] < time:
+            return index
+    return index
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Chess result generator script.",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-v', '--validation_path', default='./../data/validation/', help="Validation path")
+    parser.add_argument('-o', '--output_path', default='./../data/validation/', help="Output path")
+    parser.add_argument('-f', '--file_names', action='append', required=True, help="File names")
+    args = vars(parser.parse_args())
+
+    names = args['file_names']
+    files = []
+
+    for file_name in names:
+        files.append(f'{args["validation_path"]}results_{file_name}.csv')
+
+    for i in range(len(files)):
+        df = pd.read_csv(files[i])
+
+        result_df = split_in_matches(df)
+
+        f1_results = []
+        acc_results = []
+        elo = []
+        model = []
+        new_df = None
+        for game in result_df:
+            game = game.reset_index(drop=True)
+            end = move_play_time(game, 30.0)
+            if new_df is None:
+                new_df = game[10:end+1]
+            else:
+                new_df = new_df.append(game[10:end+1])
+
+        new_df['real_elo'] = new_df['white_elo'] if not new_df['turn'] else new_df['black_elo']
+        elos = []
+        for e in list(new_df['real_elo']):
+            elos.append((e // 10) * 10)
+        new_df['fixed_elo'] = elos
+
+        for e in list(new_df['fixed_elo'].unique()):
+            filtered_df = new_df[new_df['fixed_elo'] == e]
+            f1_results.append(metrics.f1_score(filtered_df['real'], filtered_df['predicted'], average='weighted'))
+            acc_results.append(metrics.f1_score(filtered_df['real'], filtered_df['predicted'], average='micro'))
+            elo.append(e)
+            model.append(names[i])
+
+        metric_df = pd.DataFrame({'accuracy': acc_results, 'f1_weighted': f1_results, 'elo': elo, 'model': model})
+        metric_df.to_csv(f'{args["validation_path"]}metrics2_{names[i]}.csv')
